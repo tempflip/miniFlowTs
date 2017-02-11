d8b73368e750246cdfee3ff361c7a8a1f32593a6
Peter Tempfli <tempflip@gmail.com>
2017-02-11 14:06:11 +0100

    w

--- miniFlow.ts
+++ miniFlow.ts
@@ -5,10 +5,14 @@ class MfNode {
 	inboundNodes : MfNode[];
 	outboundNodes : MfNode[];
 
-	constructor(value : number) {
-		this.value = value;
-		this.inboundNodes = [];
+	constructor(inboundNodes : MfNode[]) {
+		this.value = null;
+		this.inboundNodes = inboundNodes;
 		this.outboundNodes = [];
+
+		for (var node of this.inboundNodes) {
+			node.outboundNodes.push(this);
+		}
 	}
 
 }
@@ -16,9 +20,32 @@ class MfNode {
 class MfInput extends MfNode {
 
 	constructor(value : number) {
-		super(value);
+		super([]);
+		this.value = value;
 	}
 
 
 }
+
+class MfLinear extends MfNode {
+
+	constructor(xNode, wNode, bNode) {
+		super([xNode, wNode, bNode]);
+	}
+}
+
+
+///// test
+var x = new MfInput(10);
+var w1 = new MfInput(4);
+var b1 = new MfInput(5);
+
+var w2 = new MfInput(0.1);
+var b2 = new MfInput(0.2);
+
+var lin1 = new MfLinear(x, w1, b1);
+var lin2 = new MfLinear(lin1, w2, b2);
+
+console.log(lin1.inboundNodes);
+console.log(lin2.inboundNodes);
 
